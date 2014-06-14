09712b8e31346fb52ff2ea29e4635ad363b4813f
jia1997 <xiaoyi11788@gmail.com>
2014-06-13 22:04:00 -0400

    edits

--- Cards/Table.java
+++ Cards/Table.java
@@ -6,35 +6,37 @@ import javax.swing.*;
 public class Table extends JFrame{
 
     private int numPlays, numDraws;
+    
     private Deck deck; //deck to draw from
     private Discard discard; //discarded cards (action,goals)
     private Goal goal;
-    private NewRule newRule;
-    private Player player1;
-    private Player player2;
     private ArrayList<NewRule> newRules;
+    private ArrayList<Player> players;
+    
+    //Gui stuff
     JPanel pane = new JPanel();
     JButton instructions = new JButton("Instructions");
     JButton play = new JButton("Play Game!");
 
-    private boolean playerTurn;
-    // private Hand player1, player2;
+    //private boolean playerTurn;
     
     public Table(){
-	//Gui stuff
-	super("Fluxx, the Game");
-	setBounds(0,0,720,720);
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	Container container = this.getContentPane();
-	container.add(pane);
-	pane.add(play);
-	pane.add(instructions);
-	setVisible(true);
+        //Gui stuff
+        super("Fluxx, the Game");
+        setBounds(0,0,720,720);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container container = this.getContentPane();
+        container.add(pane);
+        pane.add(play);
+        pane.add(instructions);
+        setVisible(true);
+        
         numPlays = 1;
         numDraws = 1;
         deck = new Deck();
         discard = new Discard();
         newRules = new ArrayList<NewRule>();
+        players = new ArrayList<Player>();
 	
 	// playerTurn = Math.random() > 0.5;
         // player1 = new Hand();
@@ -47,11 +49,11 @@ public class Table extends JFrame{
     }
 
     public void setNumPlays(int i){
-	numPlays = i;
+        numPlays = i;
     }
 
     public void setNumDraws(int i){
-	numDraws = i;
+        numDraws = i;
     }
     
     public Deck getDeck(){
@@ -67,16 +69,8 @@ public class Table extends JFrame{
     }
     
     public ArrayList<NewRule> getNewRule(){
-        return newRule;
+        return newRules;
     }
-
-    // public ArrayList<Card> getPlayed1(){
-    //     return player1.getPlayed();
-    // }
-    
-    // public ArrayList<Card> getPlayed2(){
-    //     return player2.getPlayed();
-    // }
 
     // public Hand getPlayer(boolean player){
     // 	if(player == true)
@@ -86,9 +80,8 @@ public class Table extends JFrame{
     // }
 
     public static void main(String[]args){
-        // Table table = new Table();
-        // Deck dec = new Deck();
-        // System.out.println(dec.toString());
-	Table table = new Table();
+        Table table = new Table();
+        //Deck dec = new Deck();
+        //System.out.println(dec.toString());
     }
 }
